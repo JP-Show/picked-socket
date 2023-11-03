4cf4438a91c570af687e6bd1cda8b905bcf422e5
Andre Luiz <andreluiz.jpofi@gmail.com>
2023-11-02 22:00:13 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            var mc = new System.Management.ManagementClass("Win32_Processor");
+            var moc = mc.GetInstances();
+
+            foreach (System.Management.ManagementObject mo in moc)
+            {
+                foreach (var m in mo.Properties)
+                {
+                    Console.WriteLine(m.Name); 
+                }
+                
+                Console.WriteLine(mo.Properties["SocketDesignation"].Value);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Ocorreu um erro ao tentar obter informações do processador: " + e.Message);
+        }
+    }
+}
